266aa9e57d30e0c12a6a41bab5a4bd6a3916df0b
Kailash0303 <kailash.tak0303@gmail.com>
2025-02-26 11:10:47 +0530

    updated search feature in nav-bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   const [visible , setVisible] = useState(false);
 
-  const {setShowSearch} =useContext(ShopContext)
+  const {setShowSearch , getCartCount } =useContext(ShopContext)
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -51,7 +51,7 @@ const Navbar = () => {
 
         <Link to='/cart' className="relative">
             <img src={assets.cart_icon} alt="cart_icon" className="w-5 min-w-4" />
-            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full text-[8px]">10</p>
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full text-[8px]">{getCartCount()}</p>
         </Link>
 
         <img onClick={()=>setVisible(true)}  src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
